9b1101aa59e4a27bc32fbf4a4fd293b6d9ec89b4
orlando2030 <orlando2030@gmail.com>
2021-06-12 19:45:42 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrientacaoObjetosCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
