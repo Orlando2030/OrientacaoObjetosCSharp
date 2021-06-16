77d532986c20392c739fdcc48c4987de81675883
orlando2030 <orlando2030@gmail.com>
2021-06-16 13:09:55 -0300

    Commit Classes Abstratas e Interfaces

---
+++ ClassesAbstratas/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ClassesAbstratas
+{
+ class Program
+    {
+        
+        // Diferença entre Interface e Classe Abstrata
+        // A classe Abstrata pode ter uma implementação que serve de basae para outas classes
+        // A In=terface é um contrato que defini o que tem que ser feito
+        static void Main(string[] args)
+        {
+            // clase abstrata não pode ser instanciada só herdada
+            // como no exemplo a baixo quanto tentamos implementar a classe Pagamento,
+            // temos erro
+            //var pagamento = new Pagamento();
+            //pagamento.Pagar(100);
+
+            var pagamentoB = new PagamentoBoleto();
+            pagamentoB.Pagar(100.00);
+
+            var pagamentoC = new PagamentoCartao();
+            pagamentoC.Pagar(200.00);            
+        }
+
+
+        // A classe pagamento está como abstrata, pq de fato ela não faz nada
+        // Ela é a classe mae dos outros tipod de pagamento e não a a necessidade 
+        // ser instanciada
+        public abstract class Pagamento : IPagamento
+        {
+            public DateTime Vencimento { get;set; }
+
+            // O virtal no metodo siginfica que o mesmo pode ser sobrescrito
+            // quando implementedo nas classes fillhas
+            public virtual void Pagar(double valor)
+            {
+                Console.WriteLine("Pago o valor de "+ valor);
+            }
+        }
+
+        public class PagamentoBoleto : Pagamento
+        {
+            // Para o metodo ser sobrescrito da classe mae nas classes filhas
+            // é necessario colocar o override
+             public override void Pagar(double valor)
+            {
+                Console.WriteLine("Pago o valor de "+ valor);
+            }
+
+        }
+
+        public class PagamentoCartao : Pagamento
+        {
+            // Para o metodo ser sobrescrito da classe mae nas classes filhas
+            // é necessario colocar o override            
+            public override void Pagar(double valor)
+            {
+                Console.WriteLine("Pago o valor de "+ valor);
+            }
+
+        }
+
+        public interface IPagamento
+        {
+            DateTime Vencimento { get; set; }
+
+            void Pagar(double valor);
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pagamento = new Pagamento();
+            pagamento.Pagar(100);
+        }
+
+
+        // Na classe Pagamento esta sendo implementada a Interface IPagamento
+        // Diferente da herança a interface é obrigado a implementear as propriedade e os metodos
+        // da interface
+        public class Pagamento : IPagamento
+        {
+            public DateTime Vencimento { get;set; }
+
+            public void Pagar(double valor)
+            {
+                Console.WriteLine("Pago o valor de "+ valor);
+            }
+        }
+
+        // Interface = Contrato, define o que a classe Pagamento vai fazer
+        // Mas não fala como será feito
+        public interface IPagamento
+        {
+            DateTime Vencimento { get; set; }
+
+            void Pagar(double valor);
+        }
+    }
+}
